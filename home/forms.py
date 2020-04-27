2b7276e8da2b330f715083daedd561a378d1e853
kaylaramirez <ram.kayla@gmail.com>
2020-04-26 21:38:18 -0500

    deleted some unnecessary files and added base code for contact page

---
+++ home/forms.py
@@ -0,0 +1,14 @@
+"""
+forms for home app
+"""
+
+from django import forms
+
+class ContactForm(forms.Form):
+    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={
+        'class': 'form-control first-name'}))
+    last_name = forms.CharField(required=True, widget=forms.TextInput(attrs={
+        'class': 'form-control last-name'}))
+    #email_address =
+    #subject =
+    #message =
--- home/views.py
+++ home/views.py
@@ -4,15 +4,26 @@ from django.contrib import messages
 from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
+from home.forms import ContactForm
+
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'home/home.html'
 
     def get_context_data(self, **kwargs):
         kwargs['welcome_message'] = 'Welcome User!'
-
         return super().get_context_data(**kwargs)
 
+
+    def get(self, request, *args, **kwargs):
+        return self.render_to_response(self.get_context_data(**kwargs))
+
+class ContactView(TemplateView):
+    template_name = 'home/contact.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['contact_form'] = ContactForm()
+        return super().get_context_data(**kwargs)
 
     def get(self, request, *args, **kwargs):
         return self.render_to_response(self.get_context_data(**kwargs))
